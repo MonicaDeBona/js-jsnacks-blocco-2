0d938d9b6727ac02760fadc36a2acc3837144a82
MonicaDeBona <debona.monica@outlook.it>
2022-11-25 16:50:33 +0100

    Update snack3

--- js3/script.js
+++ js3/script.js
@@ -3,9 +3,13 @@
 const newArray = [ 1, 3, 8, 4, 7, 8, 4, 5, 3, 2, 4, 5, 7, ]
 
 let somma = 0;
+let average;
+
 
 for (let i = 0; i < 10; i++) {
     somma += newArray[i];
+    average = somma / 10;
 }
 
-console.log(somma);
+console.log(average);
+
