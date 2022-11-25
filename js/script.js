3fb205a6ac54288b89a26ca921580c5d5781059e
MonicaDeBona <debona.monica@outlook.it>
2022-11-25 15:46:43 +0100

    Update

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all'utente di inserire un numero,
+// se è dispari inseriscilo nell'array.
+
+
+const newArray = [ ]
+
+
+
+for (let i = 0; i < 10; i++) {
+    number = parseInt(prompt('Inserisci un numero'));
+    if (number%2 === 1) {
+        newArray.push(number);
+        console.log(newArray);
+    }
+}
+
