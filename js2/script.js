7c313c1e698fb7519e73eb68c7e22185daf70b16
MonicaDeBona <debona.monica@outlook.it>
2022-11-25 16:04:03 +0100

    Add snack2

---
+++ js2/script.js
@@ -0,0 +1,9 @@
+// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+    
+    number = parseInt(prompt('Inserisci un numero'));
+    if (number%2 === 0) {
+        console.log(number);
+    } else (
+        console.log(number + 1)
+    )
